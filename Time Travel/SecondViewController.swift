c337c77dc8398e04d96360811fb2956817234cf0
dragosrobertn <hello@dragosneagu.com>
2017-02-26 20:16:30 +0000

    Add the view for Time Traveling.

--- Time Travel/SecondViewController.swift
+++ Time Travel/SecondViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var Label2: UILabel!
+    @IBOutlet weak var Label3: UILabel!
+    @IBOutlet weak var Label4: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        showARandomYear()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAnotherYear(sender: UIButton) {
+        showARandomYear()
+    }
+    
+    func showARandomYear() {
+        let year = Utilities().GetRandomYear()
+        Label1.text = Utilities().GetLetterAtIndex(year, location:0)
+        Label2.text = Utilities().GetLetterAtIndex(year, location:1)
+        Label3.text = Utilities().GetLetterAtIndex(year, location:2)
+        Label4.text = Utilities().GetLetterAtIndex(year, location:3)
+
     }
 
 
